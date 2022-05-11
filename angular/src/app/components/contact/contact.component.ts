c81d7d3ac2b5e1b658f4b0d0526bf8b2aaf77b56
DROZD\DROZD <michaldr1@wp.pl>
2022-05-11 06:10:18 +0200

    praca na strona kontaktowa

--- angular/src/app/components/contact/contact.component.ts
+++ angular/src/app/components/contact/contact.component.ts
@@ -20,6 +20,10 @@ export class ContactComponent implements OnInit {
   markerPositions: google.maps.LatLngLiteral[] = [];
   center: google.maps.LatLngLiteral = {lat: 50.048924580103346, lng: 21.981803279444392};
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  fnameFormControl = new FormControl('', [Validators.required]);
+  lnameFormControl = new FormControl('', [Validators.required]);
+  phoneFormControl = new FormControl('', [Validators.required]);
+  messageFormControl = new FormControl('', [Validators.required]);
 
 
   constructor(http: HttpClient) {
